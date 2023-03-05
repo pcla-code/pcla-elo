8a0237139c986b02a9bf6e5b863276877d122f37
Michael Mogessie <7068628+michaelmogessie@users.noreply.github.com>
2023-03-05 15:29:34 -0500

    ELO-001

--- elo/elo_fitter.py
+++ elo/elo_fitter.py
@@ -1,19 +1,33 @@
-# import numpy as np
-# import pandas as pd
-#
-#
-# def fit_elo(student_item_scores, k=1):
-#     students = pd.unique(student_item_scores[:, 0])
-#     items = pd.unique(student_item_scores[:, 1])
-#     abilities = dict(zip(students, np.zeros(len(students), np.double)))
-#     difficulties = dict(zip(items, np.zeros(len(items), np.double)))
-#     probs = dict(zip(students, [dict(zip(items, np.full(len(items), 0.5))) for _ in range(len(students))]))
-#
-#     for row in student_item_scores:
-#         ability = abilities[row[0]]
-#         difficulty = difficulties[row[1]]
-#         correct = row[2]
-#         prob = probs[row[0]][row[1]]
-#         abilities[row[0]] = ability + (k * (correct - prob))
-#         difficulties[row[1]] = difficulty + (k * (prob - correct))
-#         probs[row[0]][row[1]] = 1 / (1 + np.exp(-(ability - difficulty)))
+import sys
+from elo_runner import run_elo
+
+
+def fit_elo(student_item_scores, step=0.01):
+    """
+    Searches the optimal k for a given list of students, items, and scores and returns student abilities, item difficulties, the optimal k and
+    the Root Mean Squared Error (RMSE) of predictions for that optimal k. This function iteratively calls the run_elo function by varying the values of k
+    between 0 and 1, with a default step of 0.01.
+    :param student_item_scores: a numpy array of shape mxn, where the fist column is the student's name, the second column is the item's name and the third
+    column is the student's score on that item. A score of 1 is correct and a score of 0 is incorrect.
+    :param step: an optional parameter that informs the algorithm by how much to increase k on each iteration. The default value of step is 0.01.
+    :return: a dictionary of the abilities of students, a dictionary of the difficulties or items, the optimal k, and
+    the Root Mean Squared Error (RMSE) of predictions.
+    """
+
+    if step < 0 or step > 1:
+        raise ValueError("step must be between 0 and 1. You provided ", step)
+
+    k = 0
+    optimal_k = 0
+    lowest_root_mean_squared_error = sys.float_info.max
+    optimal_abilities = []
+    optimal_difficulties = []
+    while k < 1:
+        abilities, difficulties, root_mean_squared_error = run_elo(student_item_scores, k)
+        if root_mean_squared_error < lowest_root_mean_squared_error:
+            optimal_k = k
+            lowest_root_mean_squared_error = root_mean_squared_error
+            optimal_abilities = abilities
+            optimal_difficulties = difficulties
+        k += step
+    return optimal_abilities, optimal_difficulties, optimal_k, lowest_root_mean_squared_error
--- elo/test_elo_runner.py
+++ elo/test_elo_runner.py
@@ -1,94 +1,15 @@
 from unittest import TestCase
-from elo_runner import run_elo
-import numpy as np
+
 from numpy.testing import assert_almost_equal
 
+from base_test_case import BaseTestCase
+from elo_runner import run_elo
 
-class Test(TestCase):
+
+class Test(TestCase, BaseTestCase):
 
     def test_run_elo_nine_students_nine_items(self):
         # given
-        student_item_scores = np.array([['good-1', 'easy-1', 1],
-                                        ['good-1', 'easy-2', 1],
-                                        ['good-1', 'easy-3', 1],
-                                        ['good-1', 'med-1', 1],
-                                        ['good-1', 'med-2', 1],
-                                        ['good-1', 'med-3', 1],
-                                        ['good-1', 'hard-1', 1],
-                                        ['good-1', 'hard-2', 1],
-                                        ['good-1', 'hard-3', 0],
-                                        ['good-2', 'easy-1', 1],
-                                        ['good-2', 'easy-2', 1],
-                                        ['good-2', 'easy-3', 1],
-                                        ['good-2', 'med-1', 1],
-                                        ['good-2', 'med-2', 1],
-                                        ['good-2', 'med-3', 1],
-                                        ['good-2', 'hard-1', 1],
-                                        ['good-2', 'hard-2', 0],
-                                        ['good-2', 'hard-3', 0],
-                                        ['good-3', 'easy-1', 1],
-                                        ['good-3', 'easy-2', 1],
-                                        ['good-3', 'easy-3', 1],
-                                        ['good-3', 'med-1', 1],
-                                        ['good-3', 'med-2', 1],
-                                        ['good-3', 'med-3', 1],
-                                        ['good-3', 'hard-1', 0],
-                                        ['good-3', 'hard-2', 0],
-                                        ['good-3', 'hard-3', 0],
-                                        ['med-1', 'easy-1', 1],
-                                        ['med-1', 'easy-2', 1],
-                                        ['med-1', 'easy-3', 1],
-                                        ['med-1', 'med-1', 1],
-                                        ['med-1', 'med-2', 1],
-                                        ['med-1', 'med-3', 0],
-                                        ['med-1', 'hard-1', 0],
-                                        ['med-1', 'hard-2', 0],
-                                        ['med-1', 'hard-3', 0],
-                                        ['med-2', 'easy-1', 1],
-                                        ['med-2', 'easy-2', 1],
-                                        ['med-2', 'easy-3', 1],
-                                        ['med-2', 'med-1', 1],
-                                        ['med-2', 'med-2', 0],
-                                        ['med-2', 'med-3', 0],
-                                        ['med-2', 'hard-1', 0],
-                                        ['med-2', 'hard-2', 0],
-                                        ['med-2', 'hard-3', 0],
-                                        ['med-3', 'easy-1', 1],
-                                        ['med-3', 'easy-2', 1],
-                                        ['med-3', 'easy-3', 1],
-                                        ['med-3', 'med-1', 0],
-                                        ['med-3', 'med-2', 0],
-                                        ['med-3', 'med-3', 0],
-                                        ['med-3', 'hard-1', 0],
-                                        ['med-3', 'hard-2', 0],
-                                        ['med-3', 'hard-3', 0],
-                                        ['bad-1', 'easy-1', 1],
-                                        ['bad-1', 'easy-2', 1],
-                                        ['bad-1', 'easy-3', 0],
-                                        ['bad-1', 'med-1', 0],
-                                        ['bad-1', 'med-2', 0],
-                                        ['bad-1', 'med-3', 0],
-                                        ['bad-1', 'hard-1', 0],
-                                        ['bad-1', 'hard-2', 0],
-                                        ['bad-1', 'hard-3', 0],
-                                        ['bad-2', 'easy-1', 1],
-                                        ['bad-2', 'easy-2', 0],
-                                        ['bad-2', 'easy-3', 0],
-                                        ['bad-2', 'med-1', 0],
-                                        ['bad-2', 'med-2', 0],
-                                        ['bad-2', 'med-3', 0],
-                                        ['bad-2', 'hard-1', 0],
-                                        ['bad-2', 'hard-2', 0],
-                                        ['bad-2', 'hard-3', 0],
-                                        ['bad-3', 'easy-1', 0],
-                                        ['bad-3', 'easy-2', 0],
-                                        ['bad-3', 'easy-3', 0],
-                                        ['bad-3', 'med-1', 0],
-                                        ['bad-3', 'med-2', 0],
-                                        ['bad-3', 'med-3', 0],
-                                        ['bad-3', 'hard-1', 0],
-                                        ['bad-3', 'hard-2', 0],
-                                        ['bad-3', 'hard-3', 0]])
         expected_abilities = {'good-1': 1.1598,
                               'good-2': 0.3651,
                               'good-3': -.1206,
@@ -107,8 +28,10 @@ class Test(TestCase):
                                  'hard-1': 1.4361,
                                  'hard-2': 1.6958,
                                  'hard-3': 1.9333}
+
         # when
-        actual_abilities, actual_difficulties = run_elo(student_item_scores)
+        actual_abilities, actual_difficulties, _ = run_elo(self.student_item_scores)
+
         # then
         for student in actual_abilities.keys():
             assert_almost_equal(actual_abilities[student], expected_abilities[student], decimal=4,
